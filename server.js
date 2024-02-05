b3dde42a3e1ce83208c7699952d9a5e01a618bdd
HarmonyMorse <harmonythedev@gmail.com>
2024-02-05 07:50:44 -0800

    Add middleware and router

--- server.js
+++ server.js
@@ -8,7 +8,20 @@ const express = require("express");
 
 const app = express();
 
+const cors = require('cors')
+const morgan = require('morgan')
+
+// Router
+const disneyAdultsRouter = require("./routes/disneyAdults");
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
 // Routes:
+app.use("/", disneyAdultsRouter);
 
 
 app.get("/", (req, res) => {
