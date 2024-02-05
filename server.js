345a910aefd7972267ba919daf4be0c304b9b8f1
ninaraynor <ninaraynor@gmail.com>
2024-02-05 09:35:14 -0500

    Set up Express

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const { PORT } = process.env;
+
+const express = require("express");
+
+const app = express();
+
+// Routes:
+
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+
